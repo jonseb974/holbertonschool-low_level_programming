86f836623ec40a03ceb73529c8a156072c363536
jonseb974 <5151@holbertonschool.com>
2022-06-06 14:37:25 -0700

    print3

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* print a string */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 
 }
