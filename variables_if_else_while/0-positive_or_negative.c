a03ffb31c4e2d9629e10617bcd49a0806b67a007
jonseb974 <5151@holbertonschool.com>
2022-06-07 05:40:16 -0700

    random number8

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	if ( n > 0) {
-		printf("is positive\n");
+		printf("is positive");
 	}
 	if (n == 0) { 
-		printf("is zero\n");
+		printf("is zero");
 	}
 	else {
-		printf("is negative\n");
+		printf("is negative");
 	}
 
 	return (0);
