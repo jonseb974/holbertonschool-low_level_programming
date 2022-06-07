cccc244585f0cf5d52a5a8e865741ababb2dc30f
jonseb974 <5151@holbertonschool.com>
2022-06-07 05:38:11 -0700

    random number7

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,12 +12,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if ( n > 0)
-		{
+	if ( n > 0) {
 		printf("is positive\n");
 	}
-	if (n == 0)
-		{ 
+	if (n == 0) { 
 		printf("is zero\n");
 	}
 	else {
