af10c11d457fde21e0c99092792597e3af735da3
jonseb974 <5151@holbertonschool.com>
2022-06-07 01:41:41 -0700

    print sizes2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,6 @@
  */
 
 int main(void)
-
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
@@ -14,5 +13,4 @@ int main(void)
 	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of float: %d byte(s)\n", sizeof(float));
 	return (0);
-
 }
