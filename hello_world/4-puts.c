c04e11cb5930960556ae90573c06e4fb08912f4e
jonseb974 <5151@holbertonschool.com>
2022-06-07 01:10:16 -0700

    first c sript3

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- *Description : main_void
+ *Description :main_void
  *  
- * @puts: print_a_string. 
+ * @puts:print_a_string. 
  *  
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
