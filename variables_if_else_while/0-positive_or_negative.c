e95912ae1dc357270be6d7b451d21a01fae8c2f7
jonseb974 <5151@holbertonschool.com>
2022-06-07 06:32:37 -0700

    random_numb3

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point   
+ * main - Entrypoint   
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	srand(time(0));
+	srand (time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0) 
 		printf("%d is positive\n", n);
