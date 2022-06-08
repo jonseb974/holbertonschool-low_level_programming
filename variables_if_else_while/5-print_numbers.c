0b6f7be32d1f730240d84f90fda2904a400efa60
jonseb974 <5151@holbertonschool.com>
2022-06-08 08:32:39 -0700

    list of numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main - print numbers from 0 to 10
+*
+*Return:0
+*/
+int maim(void)
+{
+	int i ;
+	for (i=0; i < 10; x++)
+		printf("%d", i);
+	{
+		printf("\n");	
+	}
+	return (0);
+}
