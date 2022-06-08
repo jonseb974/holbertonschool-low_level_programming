5df8d44c36d09168cc7ea5d32c8f900843f05021
jonseb974 <5151@holbertonschool.com>
2022-06-08 03:49:20 -0700

    print the alphabe2t

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,13 @@
 /**
  * main -program that prints the alphabet in lowercase
  * 
- * return:0
+ * Return: 0
  */
 int main(void)
 {
 	char ch;
 	for(ch = 'a'; ch <= 'z'; ch++){
-		putchar(ch);
+		putchar("ch\n");
 	}					    	
 return (0);
 }
