6fa5adaa9e2ff79165451e6a7f73af22c0b027bc
jonseb974 <5151@holbertonschool.com>
2022-06-08 04:06:41 -0700

    alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,16 +3,16 @@
 #include <time.h>
 /**
  * main -program that prints the alphabet in lowercase
- * 
  * Return: 0
  */
 int main(void)
 {
 	char ch;
-	for(ch = 'a'; ch <= 'z'; ch++){
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
 	}
-	putchar('\n');	
+	putchar('\n');
 	return (0);
 }
 
