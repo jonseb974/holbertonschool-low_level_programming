25ff33e1069d6b66e66b786dca396453a3bc61e7
jonseb974 <5151@holbertonschool.com>
2022-06-08 11:05:43 -0700

    print digit number

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints digits of base 10
+ * from 0, with new line
+ * Return:0
+ */
+int main(void)
+
+{
+	int i;
+	for(i=48; i<57; i++)
+		putchar(i);
+	i++;
+	{
+		printf("\n");
+	
+	}
+	return (0);
+}
