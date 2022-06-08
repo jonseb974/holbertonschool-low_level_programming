c18c46d95c2706f2b3b4a1ea329365dab33d2ee5
jonseb974 <5151@holbertonschool.com>
2022-06-08 01:04:33 -0700

    second random_2

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -17,11 +17,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5) {
-		printf("Last digit of %d\n is greater than 5", n);
+		printf("Last digit of %d is greater than 5\n", n);
 	}
-	else if (n < 6 && != 0) {
-		printf("Last digit of %d and is less than 6 and not 0");
+	else if (n < 6 && n != 0) {
+		printf("Last digit of %d and is less than 6 and not 0\n");
 	}else
-		printf("Last digit of %d is 0 and is 0");
+		printf("Last digit of %d is 0 and is 0\n");
 	return (0);
 }
