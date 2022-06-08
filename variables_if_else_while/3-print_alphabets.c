6a7091777a1af7d3fd5993ac3d91a988543c017a
jonseb974 <5151@holbertonschool.com>
2022-06-08 05:00:28 -0700

    alphatet_tree

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>      
+#include <stdlib.h>                                                                                               
+#include <time.h>                                                                                                 
+/**                                                                                                               
+* main -program that prints the alphabet in lowercase
+* and uppercase, followed by new line.                                                            
+* Return: 0                                                                                                       */                                                                                                               
+
+int main(void)                                                                                                   
+
+ {
+char ch;
+char maj;
+char minus;
+
+while (ch >= 65 || ch <= 90)
+{
+	putchar(minus);
+} while (ch >= 97 || ch <= 122)
+{
+	putchar(maj);
+} 
+	putchar('\n');
+return (0);
+}   
