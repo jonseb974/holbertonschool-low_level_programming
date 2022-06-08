1d856eef8786ed849e3973359028159747d734d1
jonseb974 <5151@holbertonschool.com>
2022-06-08 11:55:24 -0700

    reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print alphabet reverse sense
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char i = 'z';
+	while (i >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	
+	{
+		putchar('\n');
+		return (0);
+	
+	}
+}
