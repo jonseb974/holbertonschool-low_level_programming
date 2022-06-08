a4e303884eb7fdb57d0a62b3e060caf5c9d116db
jonseb974 <5151@holbertonschool.com>
2022-06-08 01:01:04 -0700

    second random1

---
+++ 1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* 
+ * *main - random number to the variable n each time it is executed
+ * *if the last digit of n is greater than 5: 
+ * *the string and is greater than 5
+ * *if the last digit of n is 0: the string and is 0
+ * *if the last digit of n is less than 6 and not 0: the string and 
+ * *is less than 6 and not 0 followed by a new line
+ * *return:0
+ * */
+int main(void)
+{
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5) {
+		printf("Last digit of %d\n is greater than 5", n);
+	}
+	else if (n < 6 && != 0) {
+		printf("Last digit of %d and is less than 6 and not 0");
+	}else
+		printf("Last digit of %d is 0 and is 0");
+	return (0);
+}
--- 1-last_digt.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#main - assign a random number to the variable n each time it is executed
-#if the last digit of n is greater than 5: the string and is greater than 5
-#if the last digit of n is 0: the string and is 0
-#if the last digit of n is less than 6 and not 0: the string and is less 
-#than 6 and not 0followed by a new line
-
-
