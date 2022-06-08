fb46623cd72785a638c6b1702fc86e5a1594b5fa
jonseb974 <5151@holbertonschool.com>
2022-06-08 02:21:58 -0700

    last number_random_3

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- * *main - random number to the variable n each time it is executed
- * *if the last digit of n is greater than 5:
- * *the string and is greater than 5
- * *if the last digit of n is 0: the string and is 0
- * *if the last digit of n is less than 6 and not 0: the string and
- * *is less than 6 and not 0 followed by a new line
- * *return:0
- * */
+/**
+ * main - random number to the variable n each time it is executed
+ * if the last digit of n is greater than 5:
+ * the string and is greater than 5
+ * if the last digit of n is 0: the string and is 0
+ * if the last digit of n is less than 6 and not 0: the string and
+ * is less than 6 and not 0 followed by a new line
+ * Return:0
+ */
 int main(void)
 {
 	int n;
 	int m;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-	if (m > 5) {
+	if (m > 5) 
 		printf("Last digit of %d is %d and is greater than 5\n", n,m);
-	}else if (m < 6 && m != 0) {
+	else if (m < 6 && m != 0) 
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,m);
-	}else
+	else
 		printf("Last digit of %d is %d and is 0\n",n,m);
 	return (0);
 }
