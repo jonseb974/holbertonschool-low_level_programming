5d79a348265f3c5c670eeb6cf481ebd80bd935ac
jonseb974 <5151@holbertonschool.com>
2022-06-08 07:54:44 -0700

    exclude e_q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>                                                                                               
+#include <time.h>                                                                                                                                        
+/**                                                                                                                
+ * main -program that prints the alphabet in lowercase                                               
+ * and uppercase with new lines
+ * Return: 0                                                                                                        */ 
+
+int main()
+{
+	char ch = 97;
+	while (ch <= 122)
+	{
+		putchar(ch);
+		ch++;
+		if (ch == 101 || ch == 113)
+			ch++;
+	}
+	putchar('\n');
+	return (0);
+}
