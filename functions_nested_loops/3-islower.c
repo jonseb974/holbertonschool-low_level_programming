82ac69f789948cf4647f0e5e7b4d808301c7fb1a
jonseb974 <5151@holbertonschool.com>
2022-06-09 21:42:01 +0200

    return 1or 0 _2

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include <ctype.h>
 /**
-*function_islower-return 1 or 0.
-*@c is an ascii integer
+* _islower-return 1 or 0.
+* @c: is an ascii integer
 *
-* Return 1 or 0
+* Return: 1 or 0
 */
 
 int _islower(int c)
 {
 	if (c >= 98 && c <= 122)
-		return(1);
+		return (1);
 	else
 		return (0);
 }
