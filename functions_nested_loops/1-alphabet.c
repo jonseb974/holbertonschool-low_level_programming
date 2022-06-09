cdc4c50557bc6029583d34a7075042f951187b53
jonseb974 <5151@holbertonschool.com>
2022-06-09 15:39:14 +0200

    print lowcase_aplhabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
 * main - print a charactere
-*
+* @print the alphabet
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 	for (i = 97; i <= 122; i++)
-		putchar(i);
-	putchar('\n');
-	return (0);
+		_putchar(i);
+	_putchar('\n');
+	
 }
 
