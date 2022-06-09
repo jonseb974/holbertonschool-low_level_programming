3c1387cea4186ac82725745d1ad05b191efb8335
jonseb974 <5151@holbertonschool.com>
2022-06-09 12:56:22 +0200

    first code

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print a charactere
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+	int i;
+	print_alphabet();
+	return (0);
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,3 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-#include "main.h"
-
 int _putchar()
 
-#endif
+
