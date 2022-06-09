5a7be36997fb95d29dd295564b58c94f5cf23c53
jonseb974 <5151@holbertonschool.com>
2022-06-09 21:56:49 +0200

    print upper or lowercase

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* -isalpha check alphabetic character
+*
+*@c: ascii interger lower and uppwer case.
+*
+*Return: 0 or 1
+*/
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122 ))
+		return (1);
+	else 
+		return (0);
+}
