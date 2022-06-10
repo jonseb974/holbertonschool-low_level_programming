c118b173d056057fc8b03c106384f339f1d64f1c
jonseb974 <5151@holbertonschool.com>
2022-06-10 19:53:31 +0200

    print_the_alphabet_lowcase

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+/**
+*print_to_98 - prints numbers from n to 98.
+*@i: input number
+*
+*Return: no return
+*/
+
+void print_to_98(int i)
+{
+	if (i < 98)
+	{
+		for (i = 0; i < 98; i++);
+		{
+			printf("%d, ", i);
+		}
+	}
+	else if (i > 98)
+	{
+		for (i = 0; i > 98; i--);
+		{
+			printf("%d, ", i);
+		}
+	}
+	if (i == 98)
+	{
+		printf("%d\n", i);
+	}
+}
