561284fcfd9a17da4558a936f74fba6410cde7bc
jonseb974 <5151@holbertonschool.com>
2022-06-10 15:05:17 +0200

    24_hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+/**
+*jack_bauer - prints ;inutes of Jack Bauer day
+*start from 00:00 to 23:59.
+*
+*Return: no return.
+*/
+
+void jack_bauer(void)
+{
+	int x = 0;
+	int j = 0;
+	
+	while (x <= 23)
+	{
+		j = 0;
+		while (j <= 59)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(':');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar('\n');
+			j += 1;
+		}
+		x += 1;
+	}
+
+}
