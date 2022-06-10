02d91bbb585b85112b35623743817935bf8cad76
jonseb974 <5151@holbertonschool.com>
2022-06-10 21:38:38 +0200

    random_numbers_posi_neg

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main -assign random number to variable 
- * if the number is greater than 0: is positive
- * if the number is 0: is zero
- * f the number is less than 0: is negative 
- * Return: 0 (Success)
- */
+* main -assign random number to variable
+* if the number is greater than 0: is positive
+* if the number is 0: is zero
+* if the number is less than 0: is negative
+* Return: 0 (Success)
+*/
 int main(void)
 {
 	int n;
 
-	srand (time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) 
-		printf("%d is positive\n", n);
-	else if (n < 0) 
-		printf("%d is negative\n", n);
-	else  
-		printf("%d is zero\n", n);
+	if (n > 0)
+	printf("%d is positive\n", n);
+	else if (n < 0)
+	printf("%d is negative\n", n);
+	else
+	printf("%d is zero\n", n);
 	return (0);
 }
