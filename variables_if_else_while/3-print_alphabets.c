c3acc671aa8b906875d85cc677f3e65389b89477
jonseb974 <5151@holbertonschool.com>
2022-06-10 21:56:41 +0200

    double_alphab_6

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,17 @@
-#include <stdio.h>      
-#include <stdlib.h>                                                                                               
-#include <time.h>                                                                                                 
-/**                                                                                                               
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
 * main -program that prints the alphabet in lowercase
-* and uppercase, followed by new line.                                                            
-* Return: 0                                                                                                       
-*/                                                                                                               
-
-int main(void)                                                                                                   
+* * and uppercase, followed by new line.
+* Return: 0
+*/
+int main(void)
 {
 	char minus = 65;
 	char maj = 97;
-       	while (maj <= 122)
+	while (maj <= 122)
+
 	{
 		putchar(maj);
 		maj++;
@@ -19,7 +19,7 @@ int main(void)
 	{
 		putchar(minus);
 		minus++;
-	} 
+	}
 	putchar('\n');
 	return (0);
-}   
+}
