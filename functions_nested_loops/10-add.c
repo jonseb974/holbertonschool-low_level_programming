f84c52ebde6df811421c7799f741d7bf061039bb
jonseb974 <5151@holbertonschool.com>
2022-06-10 16:25:04 +0200

    addition

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,2 +1,14 @@
-#include <sdtio.h>
+#include <stdio.h>
+/**
+*add - adds two integers.
+*@a: is an integer.
+*@b: is an integer.
+*Return: return 0.
+*/
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
 
+	return (sum);
+}
