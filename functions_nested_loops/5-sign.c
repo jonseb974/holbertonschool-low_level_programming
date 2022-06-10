63b895ec16ef7a1b686d280033c757136812bff0
jonseb974 <5151@holbertonschool.com>
2022-06-10 11:14:43 +0200

    sign check

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+/**
+* print_sign - print sign of a number.
+* @n: is an interger.
+* Return: 1 & + or 0 or -1 & - 
+*/
+int print_sign(int n) 
+{
+	if (n > 0)
+	{
+		putchar (43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar (45);
+		return (-1);
+	}
+	else
+	{
+		putchar (48);
+		return (0);	
+	}
+}
