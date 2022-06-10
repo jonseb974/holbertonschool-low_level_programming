127d57572eb517df96489220b827744c479fb953
jonseb974 <5151@holbertonschool.com>
2022-06-10 13:13:14 +0200

    absolute value

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+*_abs - computes abslotute value of an integer.
+*@x: integer
+*Return: int.
+*/
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		x = x * (-1);
+		return (x);
+	}
+	else
+		return (x);
+}
 
