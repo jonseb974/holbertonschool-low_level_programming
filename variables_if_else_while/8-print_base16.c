e7646a422fd661f6a97911136e36cbb3fb671932
jonseb974 <5151@holbertonschool.com>
2022-06-12 12:00:40 +0200

    hexa

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,17 +10,16 @@ int main(void)
 {
 	int i = 48;
 	int j = 'a';
+
 	while (i <= 57)
 	{
 		putchar(i);
 		i++;
-	
 	}
 	while (j <= 'f')
 	{
 		putchar(j);
 		j++;
-	
 	}
 	putchar('\n');
 	return (0);
