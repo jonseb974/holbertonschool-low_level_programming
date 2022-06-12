4a3808aa4b4f1d5248c218d03eef118fc6e9c125
jonseb974 <5151@holbertonschool.com>
2022-06-12 17:22:11 +0200

    combi

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,42 +1,29 @@
 #include <stdio.h>
 /**
 *main - print combination od two digits.
-*
-*
-*
 *Return:0
 */
 
-int main()
+int main(void)
 {
-	int j = 48;
+	int n = 48;
 	int k;
-	int c = 44;
+	int m = 44;
 
-	while (j <= 57)
+	while (n <= 57)
+		k = n + 1;
 	{
-		k = j + 1;
-
-		while (j <= 57)
+		putchar(n);
+		putchar(k);
+		if (n != 57 || k <= 57)
 		{
-			putchar(j);
-			putchar(k);
-			if (j != 57 || k != 57)
-			{
-				putchar(c);
-				putchar(32);
-			}
-			{
-				j++;
-			}
-			{
-				k++;
-			}
-			{
-				putchar('\n');
-
-			}
+			putchar(m);
+			putchar(32);
 		}
-		return (0);
+		n++;
+		k++;
 	}
+	putchar('\n');
+	return (0);
 }
+
