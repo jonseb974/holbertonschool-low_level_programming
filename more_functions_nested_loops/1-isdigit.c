de6891a840dcfc1ccc4586f96b098307a8ec96a3
jonseb974 <5151@holbertonschool.com>
2022-06-13 12:23:42 +0200

    is it a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+/**
+*_isdigit-check if the number is digit.
+*
+*Return: return 1 if is digit 0 if not.
+*
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c >= 9)
+	{
+		return (1);
+	c++;
+	}
+	else
+	{
+		return (0);
+	}
+}
