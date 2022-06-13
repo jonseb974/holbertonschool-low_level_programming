c19d859c6f01c2e25f94ce084fcdf34776cf795d
jonseb974 <5151@holbertonschool.com>
2022-06-13 13:07:19 +0200

    multiply

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*mul-return sum of multiplication of two int.
+*@a: an integer
+*@b: an integer
+*@sum: sum of multiplication of two integers.
+*Return: return 0.
+*/
+
+int mul(int a, int b)
+{
+	 mul = a * b;
+
+	printf("%d", mul);
+	return (0);
+}
