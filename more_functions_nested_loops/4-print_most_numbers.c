891d7502748c00e8377d7d06b479e7a937323104
jonseb974 <5151@holbertonschool.com>
2022-06-13 14:08:02 +0200

    print most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+/**
+*print_most_numbers-print a list of selected numbers.
+*
+*Return: no return.
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+		if (i == 2 && i == 4)
+			continue;
+		else
+			putchar (i);
+}
