8425b266871393d505f218e9d3ac15f8cf1606f3
jonseb974 <5151@holbertonschool.com>
2022-06-13 18:08:57 +0200

    some lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*print_line - print the drawnig of a line.
+*@n: is qn integer
+*
+*
+*/
+
+void print_line(int n)
+{
+	for (n = 0; n < 0; n++)
+	{
+		putchar(n);
+		putchar('_');
+	if (n <= 0)
+		putchar('\n');
+	}
+}
