fa84feec69135518ae363ce1e7d28a37b20e8bc2
jonseb974 <5151@holbertonschool.com>
2022-06-13 21:28:55 +0200

    print_line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,21 +2,24 @@
 #include <ctype.h>
 /**
 *print_line - print the drawnig of a line.
-*@n: is qn integer
+*@n: is an integer
 *
 *
 */
 
 void print_line(int n)
 {
-	int n = 0;
+	int i = 0;
 
-	while (n < 0)
+	while  (i < n)
 	{
-		putchar(n);
-		putchar(95);
-		n++;
-	if (n <= 0)
-		putchar('\n');
+		if (n > 0)
+		{
+			putchar (95);
+			i++;
+		}
+		else
+			putchar ('\n');
 	}
+	putchar ('\n');
 }
