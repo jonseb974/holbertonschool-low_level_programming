3db75a31ae5c35f5ea9a64486e7f197e4325c279
jonseb974 <5151@holbertonschool.com>
2022-06-13 17:51:35 +0200

    list 0a numbers_assen_order

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*more_numbers - print a list of numbers
+*on several lines.
+*
+*Return: return 0.
+*/
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 2 ; i++)
+	{
+		int j;
+
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+				putchar ((j / 10) + '0');
+			putchar ((j % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
