ae0f754f2359cb0a57ce1725dbb674f6e40a09eb
jonseb974 <5151@holbertonschool.com>
2022-06-13 13:25:55 +0200

    print the numbers

---
+++ more_functions_nested_loops/3-prrint_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+*print_numbers-print numbers from 0 to 9
+*Return: return 0
+*/
+
+void print_numbers(void)
+{
+	for (i = 48; i < 58 ; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+
+}
