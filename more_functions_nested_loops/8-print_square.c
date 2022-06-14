345289d8bbbccaee408bd9ddbef79a837a1520ee
jonseb974 <5151@holbertonschool.com>
2022-06-14 10:47:19 +0200

    draw_square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/*
+*print_square-make the shape of a square
+*with symbol hashtag
+*@i: integer
+@j: integer
+*Return:no return.
+*/
+void print_square(int size)
+{
+	int i;
+	int j;
+	for (i = 0; i <= size; i++)
+	{
+		for (j = 0; j <= size; j++)
+		{
+			putchar (35);
+		}
+		putchar ('\n');
+	}
+}
