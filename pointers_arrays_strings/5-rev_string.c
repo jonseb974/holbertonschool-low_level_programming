e15329646c8fdbb89dd2becea7cd5f90a7ca183e
jonseb974 <5151@holbertonschool.com>
2022-06-15 22:43:40 +0200

    reverse the string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * rev_string - print a string in reverse order
  * @s: pointer tio char
@@ -6,14 +7,14 @@
  */
 void rev_string(char *s)
 {
-	int i ;
-	int j ;
+	int i = 0;
+	int j = strlen(s);
 	int tmp;
 
-	for (i = 0; i < j/2; i++)
+	for (i = 0; i < j / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[j -i -1];
-		s[j -i -1] = tmp;
+		s[i] = s[j - i - 1];
+		s[j - i - 1] = tmp;
 	}
 }
