e2ef287a6f6a3ad243287d3d8c9407dfdbb3d01e
jonseb974 <5151@holbertonschool.com>
2022-06-15 14:12:52 +0200

    count length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+*_strlen-return the length of a string
+*@s: a char.
+*@c: a counter.
+*Returns: return length of a string.
+*/
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		c++;
+		s++;
+	}
+	return (c);
+}
