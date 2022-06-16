312288293b9c5f6947fb787cf733ac45d87995cd
jonseb974 <5151@holbertonschool.com>
2022-06-16 17:50:26 +0200

    pront list of numbers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - print an array with space and comma
+ *@a: a pointer of int q
+ *@n: a pointer of int n
+ *Returns: non return
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+
