b4be3188810a81375cede4893d059c47ba488a18
jonseb974 <5151@holbertonschool.com>
2022-06-17 17:13:37 +0200

    concatanes_string

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,14 @@
 char *_strcat(char *dest, char *src)
 {
 	int i;
+	int j;
 
-	for (i = 0; src[i] != 0; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		*(dest + i) = *(src + i);
-		*(dest + i) = '\0';
-		_strcat(dest, src);
+		i++;
+		for (j = 0; dest[j] != '\0'; j++)
+			dest[j] = src[i];
+		j++;
 	}
 	return (dest);
 }
