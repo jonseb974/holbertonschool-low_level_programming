7e13a4bd8fde4e4bedbf405cbc53f95450e213de
jonseb974 <5151@holbertonschool.com>
2022-06-19 15:31:28 +0200

    copy memory area to dest

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+/**
+ * _memcpy - copy memory area.
+ * @dest: pointer destination file.
+ * @src: pointer origin file.
+ * @n: number of character to copy.
+ * Return: dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (i < n)
+	{
+		*(dest + j) = *(src + i);
+		i--;
+		j++;
+	}
+	return (dest);
+}
