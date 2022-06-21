a07bc2bdfdc9a94488ffb7ada7d4b77a87a504c8
jonseb974 <5151@holbertonschool.com>
2022-06-21 11:28:20 +0200

    static lib

---
+++ static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - print a string.
+ * Return: no return
+ */
+#include "main.h"
+
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n
+			\t- Kevin Mitnick");
+	return (0);
+}
