7ddd634b07828f6f981b0fb599cf3eef481536c4
jonseb974 <5151@holbertonschool.com>
2022-06-21 13:27:56 +0200

    name_the_program

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print program's name
+ * @argc: integer
+ *@argv: char
+ * Return: no return.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("The name of the program is: %s", argv[i]);
+	}
+	printf("\n");
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,4 @@
+#!/bin/bash
+# readme file
+This readme is linked to argc_argv repository, for technicals purposes.
+
