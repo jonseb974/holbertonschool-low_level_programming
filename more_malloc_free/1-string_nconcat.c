a40775744a52cddc103acab7f240bb4e025dd321
jonseb974 <5151@holbertonschool.com>
2022-06-27 23:16:57 +0200

    concatenstring2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= j)
 		n = j;
 
-	str3 = malloc(sizeof(*str3) * n  + (i + 1));
+	str3 = malloc(sizeof(*str3) * n  + (i + j));
 
 	if (str3 == NULL)
 		return (NULL);
