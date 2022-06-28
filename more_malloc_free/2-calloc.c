7c8df79236801ee505150f3bfd7a3d05b18add54
jonseb974 <5151@holbertonschool.com>
2022-06-28 22:22:38 +0200

    first_calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,38 +12,22 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
 
-	i = j = 0;
+	char *j;
 
-	if (size != 0)
-		arr = malloc(nmemb * size);
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	if (malloc == NULL)
-		return (NULL);
-	_memset(arr, 0, nmemb * size)
-		return (NULL);
-	{
-	return (arr);
-}
-
-/**
- * _memo - function fills memory with constant bytes.
- * @a: memory size.
- * @b: constant bytes.
- * @c: size of bytes
- * Return: char.
- */
-
-void* _memset(char *a, char b, unsigned int c)
-{
 	unsigned int i;
 
 	i = 0;
 
-	while (i < c)
-	{
-		*(a + i) = b;
-		i++;
-		return (_memset);
-	}
+	if (size != 0)
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	j = arr;
+
+	for (i = 0; i < (nmemb * size); i++)
+		j[i] = '\0';
+
+	return (arr);
 }
