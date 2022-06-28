dfb49893720df48d9394ce87b8ac7b470008621c
jonseb974 <5151@holbertonschool.com>
2022-06-28 22:08:56 +0200

    concatenate_new

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,32 @@
 #include "main.h"
-#include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings.
- *@s1: string 1.
- *@s2: string 2.
- *@n: integer.
- *
- *Return: pointer, or new string.
+ * string_nconcat - function
+ * @s2: second str
+ * @s1: first str
+ * @n: length
+ * Return: new pointer allocated
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str3;
-	char *vide = "";
-
-	unsigned int i, j;
-
-	i = j = 0;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
-		s1 = vide;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = vide;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	j++;
-
-	if (n >= j)
-		n = j;
-
-	str3 = malloc(sizeof(*str3) * n  + (i + j));
-
-	if (str3 == NULL)
+		s2 = "";
+	for (index = 0; s1[index]; index++)
+		len++;
+	for (index = 0; s2[index]; index++)
+		len++;
+	concat = malloc(sizeof(char) * (len));
+	if (concat == NULL)
 		return (NULL);
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		str3[i] = s1[i];
-		i++;
-	j++;
-	}
-	while (s2[j] != '\0' && j < n)
-	{
-		str3[i] = s2[j];
-		i++;
-		j++;
-	}
-	str3[i] = '\0';
-	return (str3);
+	len = 0;
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+	concat[len] = '\0';
+	return (concat);
 }
