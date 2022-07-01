b25b4dc07c6a005cabac69a26591ad0582aefb0e
jonseb974 <5151@holbertonschool.com>
2022-07-01 22:15:07 +0200

    calculator

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+/**
+ * op_add - function to add
+ * @a: int
+ * @b: int
+ * Return: sum a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to substract.
+ * @a: int
+ * @b: int
+ * Return: sub a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function to multiply
+ * @a: int
+ * @b: int
+ * Return: mul a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function divide
+ * @a: int
+ * @b: int
+ * Return: div a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function modulo remainder of div
+ * @a: int
+ * @b: int
+ * Return: modulo a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
