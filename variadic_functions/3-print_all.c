81cd06b20f1254eb632313f8851b84c92e3d0ca5
jonseb974 <5151@holbertonschool.com>
2022-07-05 23:12:08 +0200

    test_print all3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
  * print_a_char- print char list
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef HEADER
 #define HEADER
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
 int _putchar(char c);
@@ -8,10 +9,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_a_char(char *separator, va_list, args);
-void print_integer(char *separator, va_list, args);
-void print_char_ptr(char *separator, va_list, args);
-void print_float(char *separator, va_list, args);
+void print_a_char(char *separator, va_list args);
+void print_integer(char *separator, va_list args);
+void print_char_ptr(char *separator, va_list args);
+void print_float(char *separator, va_list args);
 
 /**
  *struct format_types- structures formats
