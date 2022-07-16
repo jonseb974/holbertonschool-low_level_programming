eecc007628d9d7f7f1c5203ffa005718a836f4ba
jonseb974 <5151@holbertonschool.com>
2022-07-16 21:36:08 +0200

    test1_free

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - a function that free a list.
+ * @head: a pointer to the heqd node.
+ * Return: Always 0.
+ */
+void free_list(list_t *head)
+{
+	list_t tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
