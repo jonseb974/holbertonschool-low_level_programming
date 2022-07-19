4f4ed546446268b5eec4b1b5461296dc1ee39432
jonseb974 <5151@holbertonschool.com>
2022-07-19 15:03:27 +0200

    test1_task3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * set_bit - seta function that sets the value
+ * of a bit to 1 at a given index.
+ * @n: number
+ * @index: target value
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i, res;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	res = 1 << index;
+	*n = *n | res;
+	return (1);
+}
