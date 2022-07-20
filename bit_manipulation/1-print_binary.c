3c131be2686e06df13eec3b1231129dedd46f174
jonseb974 <5151@holbertonschool.com>
2022-07-20 11:07:16 +0200

    test3_task1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
-#include <math.h>
 #include <string.h>
 #include "main.h"
-/**
- * _pow_recursion - calulation of power
- * @x: number
- * @y: number
- * Return: output value.
- */
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
-}
-
 /**
  * print_binary - binary number.
  * @n: number
@@ -24,25 +8,9 @@ int _pow_recursion(int x, int y)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int power = 0;
-	int expo = 10;
-	int flag = 0;
-
-	if (n == 0)
-	_putchar(0);
-	while (expo >= 0)
+	if (n > 1)
 	{
-		power = _pow_recursion(2, expo);
-		if (n >= power)
-		{
-			_putchar('1');
-			n -= power;
-			flag = 1;
-		}
-		else if (n < power && flag == 1)
-		{
-			_putchar('0');
-		}
-		expo--;
+		print_binary(n >> 1);
+		_putchar((n & 1) +'0');
 	}
 }
