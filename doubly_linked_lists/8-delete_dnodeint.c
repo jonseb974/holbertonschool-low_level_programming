62b10b542ec103f51727ad9d0f16e624fdce4e0a
jonseb974 <5151@holbertonschool.com>
2022-07-24 18:21:16 +0200

    test2_task8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -34,9 +34,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			free(tmp);
 			return (1);
 		}
-		while  (tmp->next = NULL)
+		while  (tmp->next == NULL)
 		{
-			tmp->prev == NULL;
+			tmp->prev = NULL;
 			free(tmp);
 			return (1);
 		}
