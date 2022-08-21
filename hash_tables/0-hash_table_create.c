78a82e69bc9d81ce05e3221afe39de960386f68b
jonseb974 <5151@holbertonschool.com>
2022-08-21 12:09:15 +0200

    update_test10-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -5,30 +5,36 @@
 
 /**
  * hash_table_create - function that creates a hash table.
- * @size: size of the array.
+ * @size: size of the hash table.
  * Return: a pointer to the newly created hash table
  * or Null If something went wrong.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *h_tab = NULL;/*initialisation of the table*/
+	unsigned long int i = 0;/*size of table initialised*/
+
 	if (size < 1)
 	{
 		return (NULL); /*invalide size, Null pointer means error*/
 	}
-	hash_table_t *newTable = malloc(sizeof(hash_table_t))
+	h_tab = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-	if (!newTable)
+	if (!h_tab)
 	{
-		free(newTable);
+		free(h_tab);
 		return (NULL);
 	}
-	newTable->arraySize = size
+	h_tab->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 
-	newTable->buckets = calloc((int)size, sizeof(listNode *))
-
-	if (!newTable->buckets)
+	if (!h_tab->array)
 	{
-		free(newTable)
+		return (NULL);
 	}
+	h_tab->size = size;
+
+	while (i < size)
+		h_tab->array[i++] = NULL;
+	return (h_tab);
 }
