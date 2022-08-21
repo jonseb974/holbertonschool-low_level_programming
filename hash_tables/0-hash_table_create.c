60d9960167104c077424a076b403099e4bf88365
jonseb974 <5151@holbertonschool.com>
2022-08-21 11:25:52 +0200

    update_3

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,20 +14,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return (NULL); /*invalide size, Null pointer means error*/
 	}
-	hash_table_t *newTable = malloc(sizeof(hash_table_t));
+	hash_table_t *newTable = malloc(sizeof(hash_table_t))
 
 	if (!newTable)
 	{
 		free(newTable);
 		return (NULL);
 	}
-	newTable->arraySize = size;
+	newTable->arraySize = size
 
-	newTable->buckets = calloc((int)size, sizeof(listNode *));
+	newTable->buckets = calloc((int)size, sizeof(listNode *))
 
 	if (!newTable->buckets)
 	{
-		free(newTable);
+		free(newTable)
 	}
 
 }
