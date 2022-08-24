5183a9896fdc2951a2a345bede8262ae480c346f
jonseb974 <5151@holbertonschool.com>
2022-08-24 17:44:24 +0200

    test_4

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -23,6 +23,7 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
+		i++;
 	}
 	return (-1);
 }
