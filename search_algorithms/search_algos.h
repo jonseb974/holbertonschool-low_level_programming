0f3c2fbd79881bb93aa073ae7edbd92546390519
jonseb974 <5151@holbertonschool.com>
2022-08-24 21:41:46 +0200

    add_newproto

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -8,5 +8,7 @@
 /*prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int help_search(int *array, int value, size_t min, size_t max);
+void array_print(int *array, size_t min, size_t max);
 
 #endif
